30feb7b1ec5b2a37e2b7c71b87013b2b915939f2
Michael Walker <michael.w@quantum-brilliance.com>
2022-11-01 22:17:28 +0000

    Update QuantumDecoderAlgorithmTester.cpp

--- tests/QuantumDecoderAlgorithmTester.cpp
+++ tests/QuantumDecoderAlgorithmTester.cpp
@@ -2,105 +2,115 @@
 #include "Circuit.hpp"
 #include "xacc.hpp"
 #include "xacc_service.hpp"
+#include <cmath>
 #include <gtest/gtest.h>
+#include <math.h>
 #include <numeric>
+#include <string>
+#include <type_traits>
 
-TEST(QuantumDecoderCanonicalAlgorithmTester, checkSimple) {
+//std::map<std::string, int>
+void check_output_strings(std::vector<std::vector<float>> prob_table, std::vector<int> qubits_string, 
+                           std::vector<std::string> best_beams, std::vector<std::string> included_beams, std::vector<std::string> excluded_beams) {
+  //Set inputs
+  auto acc = xacc::getAccelerator("sparse-sim", {{"shots", 1000}});
+  auto gateRegistry = xacc::getService<xacc::IRProvider>("quantum");
+  std::cout << "getAlgorithm()" << std::endl;
+  auto simplified_decoder_algo = xacc::getAlgorithm(
+    "simplified-decoder", {{"probability_table", prob_table},
+                        {"qubits_string", qubits_string},
+                        {"qpu", acc}});
+
+  auto buffer = xacc::qalloc((int)qubits_string.size());
+  std::cout << "execute()" << std::endl;
+  simplified_decoder_algo->execute(buffer);
+
+  // Verify best beam
+  std::cout << "best beam:" ;
+  auto info = buffer->getInformation();
+  std::string max_beam = info.at("best_beam").as<std::string>();
+  std::cout << max_beam <<  std::endl;   
+  if (best_beams.size() > 0) {
+      assert(("Vector best_beams does not contain the best beam " + max_beam,std::find(best_beams.begin(),best_beams.end(),max_beam) != best_beams.end()));
+  }
+
+  // Verify returned beams
+  // Excluded beams
+  int nb_beams = info.at("nb_beams").as<int>();
+  std::vector<std::string> found_beams;
+  std::cout << "nb_beams:" << nb_beams << std::endl;
+  for (int count = 0; count < nb_beams; count++) {
+      std::string label_ = "beam_" + std::to_string(count) ;
+      std::string beam_ = info.at(label_).as<std::string>();
+      found_beams.push_back(beam_);
+      assert(("Found excluded beam " + beam_,std::find(excluded_beams.begin(),excluded_beams.end(),beam_) == excluded_beams.end()));
+      std::cout << label_ << ": " << beam_ ;
+      label_ = "beam_count_" + std::to_string(count) ;
+      std::cout << "   " << (int) info.at(label_).as<int>() << std::endl;
+  }
+  // Included beams
+  for (std::string beam_ : included_beams) {
+      assert(std::find(found_beams.begin(),found_beams.end(),beam_) != found_beams.end());
+  }
+
+  // Verify number of beams
+  int nb_timesteps_ = prob_table.size(); 
+  int nb_symbols_ = prob_table[0].size();
+  int nq_symbol_ = std::ceil(std::log2(nb_symbols_)) ;
+  
+  int upper_nb_beams = std::pow(nb_symbols_ - 1,nb_timesteps_);    // String repetitions and leading nulls removed
+  // Repetitions and leading nulls gone, remove remaining nulls
+  std::cout << "upper_nb_beams: " << upper_nb_beams << " ";
+  
+  std::cout << upper_nb_beams << " ";
+  // Remove non-leading nulls but not ending nulls
+  // Subtract number of leading non-null followed by null followed by non-null different from leading (non-null) symbol
+  if (nb_timesteps_ > 2) {
+      upper_nb_beams -= (nb_symbols_ - 1) * std::pow(nb_symbols_-2,nb_timesteps_-2) * (nb_timesteps_-2);    // Leading non-null, null , nb_timesteps_-2 non-repetitive non-null, for null at nb_timesteps_-2
+      std::cout << upper_nb_beams << " " ;
+  }
+  if (nb_timesteps_ > 4) {
+      upper_nb_beams -= (nb_symbols_ - 1) * std::pow(nb_symbols_-2,nb_timesteps_-3) * (nb_timesteps_-4);    // Two nulls minimal distance apart
+  }
+  // More generally, second null can be from 2 to nb_timesteps_-3 places after first one//. 0.5 * ((nb_timesteps-4)*(nb_timesteps-3)
+  std::cout << std::endl;
+  
+  assert(("Wrong number of beams",nb_beams < upper_nb_beams));
+
+}
+
+
+
+TEST(SimplifiedDecoderAlgorithmTester, checkSimple) {
   //Initial state parameters:
-  std::vector<std::string> alphabet = {"-","a"};
+  std::vector<int> qubits_string ;
+  std::vector<std::string> alphabet = {"-","a","b","c"};
+  int nb_symbols = alphabet.size();
+  int nq_symbol = std::ceil(std::log2(nb_symbols)) ;
 
   //Rows represent time step, while columns represent alphabets.
   //For each row, the sum of probabilities across the columns must add up to 1.
-  std::vector<std::vector<float>> probability_table{{1.0, 0.0}, {1.0, 0.0}};
+  std::vector<std::vector<float>> probability_table = {{0.0, 0.5, 0.5, 0.0}, {0.25, 0.25, 0.25, 0.25}};  
 
-  int L = probability_table.size(); // string length = number of rows of probability_table (number of columns is probability_table[0].size())
-  int S = 1; // number of qubits per letter, ceiling(log2(|Sigma|))
-  int m = 2;//2; // metric precision
-  int k = std::round(0.5 + std::log2(1 + L*(std::pow(2,m) - 1))); // number of qubits in total_metric
-  int k2 = k*(k+1)/2; // number of precision qubits needed for ae
-  int k3 = k + L; // number of qubits needed for qubits_beam_metric
-
-  //First, the qubits that aren't ancilla / can't be re-used:
-  std::vector<int> qubits_metric;
-  for (int i = 0; i < L*m; i++) {qubits_metric.emplace_back(i);} // L*m
-  std::vector<int> qubits_string;
-  for (int i = L*m; i < L*m + L*S; i++) {qubits_string.emplace_back(i);} // L*S
-  std::vector<int> qubits_init_null;
-  for (int i = L*m+L*S; i < L*(m+S+1); i++) {qubits_init_null.emplace_back(i);} // L
-  std::vector<int> qubits_init_repeat;
-  for (int i = L*(m+S+1); i < L*(m+S+2); i++) {qubits_init_repeat.emplace_back(i);} // L
-  std::vector<int> qubits_superfluous_flags; 
-  for (int i = L*(m+S+2); i < L*(m+S+3); i++) {qubits_superfluous_flags.emplace_back(i);} // L
-
-  std::vector<int> qubits_total_metric_copy;
-  for (int i = L*(m+2*S+6); i < L*(m+2*S+6) + k; i++) {qubits_total_metric_copy.emplace_back(i);} // k
-
-  std::vector<int> qubits_ancilla_adder;
-  for (int i = L*(m+2*S+6)+k; i < L*(m+2*S+6)+2*k-m; i++) {qubits_ancilla_adder.emplace_back(i);} //We need total_metric to have k = log2(1 + L*(2**m - 1)) qubits, so qubits_ancilla_adder.size() = k - m
-  std::vector<int> qubits_beam_metric;
-  for (int i = L*(m+2*S+6)+2*k-m; i < L*(m+2*S+6)+3*k-m+L; i++) {qubits_beam_metric.emplace_back(i);} // k + L 
-  std::vector<int> qubits_best_score;
-  for (int i = L*(m+2*S+6)+3*k-m+L; i < L*(m+2*S+6)+4*k-m+2*L; i++) {qubits_best_score.emplace_back(i);} //Same size as qubits_beam_metric
-
-  std::vector<int> precision_bits; 
-  for (int i = 1; i <= k; i++) {precision_bits.emplace_back(i);} //THESE ARE NOT QUBITS! {2^0, 2^1, 2^2, ...}
-  std::vector<int> evaluation_qubits;
-   for (int i = L*(m+2*S+6)+4*k-m+2*L; i < L*(m+2*S+6)+4*k-m+2*L+k2; i++) {evaluation_qubits.emplace_back(i);} //k2
-
-  //The remaining qubits can be drawn from an 'ancilla pool'. The size of this ancilla
-  //pool needs to be the maximum number of ancilla required at any one time.
-  int last_qubit_non_ancilla = evaluation_qubits[evaluation_qubits.size() - 1] + 1;//qubits_best_score[qubits_best_score.size() - 1] + 1;
-  std::vector<int> qubits_ancilla_pool;
-  int num_qubits_ancilla_pool = std::max({S+m, k-m, 3, 3*k3});
-  for (int i = 0; i < num_qubits_ancilla_pool; i++) {
-	qubits_ancilla_pool.push_back(last_qubit_non_ancilla + i);
+  int nb_timesteps = probability_table.size();  //Number of columns of probability_table // string length = number of rows of probability_table (number of columns is probability_table[0].size())
+  //int nq_symbol = 2; // number of qubits per letter, ceiling(log2(|Sigma|))
+  
+  
+  for (int i = 0; i < nb_timesteps*nq_symbol; i++) {
+      qubits_string.emplace_back(i);
   }
-  std::cout<<"num qubits = " << L*(m+3*S+6) + 4*k - m + 2*L + k2 + num_qubits_ancilla_pool  << "\n";
+  
+  std::cout<<"num qubits = " << nb_timesteps*nq_symbol << " " ;
+  std::cout << " " << qubits_string.size() << "\n";
 
-  const int BestScore = 0; //Initial best score
   int N_TRIALS = 1; //Number of decoder iterations
-
-//  const int iteration = probability_table[0].size(); //Number of columns of probability_table
-  const int iteration = probability_table.size(); //Number of rows of probability_table
-  const int num_next_letter_qubits = S;
-  const int num_next_metric_qubits = m;
 
   ////////////////////////////////////////////////////////////////////////////////////////
 
-  //Exponential search parameters
-  //Set inputs
-  const int num_scoring_qubits = qubits_metric.size();
-  auto acc = xacc::getAccelerator("sparse-sim", {{"shots", 1}});
-  auto gateRegistry = xacc::getService<xacc::IRProvider>("quantum");
-  auto quantum_decoder_algo = xacc::getAlgorithm(
-    "quantum-decoder", {{"iteration", iteration},
-                        {"probability_table", probability_table},
-                        {"qubits_metric", qubits_metric},
-                        {"qubits_string", qubits_string},
-                        {"method", "canonical"},
-                        {"BestScore", BestScore},
-                        {"N_TRIALS", N_TRIALS},
-                        {"qubits_ancilla_adder", qubits_ancilla_adder},
-                        {"qubits_init_null", qubits_init_null},
-                        {"qubits_init_repeat", qubits_init_repeat},
-                        {"qubits_superfluous_flags", qubits_superfluous_flags},
-                        {"qubits_beam_metric", qubits_beam_metric},
-                        {"qubits_ancilla_pool", qubits_ancilla_pool},
-                        {"qubits_best_score", qubits_best_score},
-                        {"qubits_total_metric_copy", qubits_total_metric_copy},
-                        {"evaluation_bits", evaluation_qubits},
-                        {"precision_bits", precision_bits},
-                        {"qpu", acc}});
-
-  auto buffer = xacc::qalloc(1 + (int)qubits_string.size() + (int)qubits_total_metric_copy.size() + (int)qubits_metric.size()
-                               + (int)qubits_best_score.size() + (int)qubits_ancilla_adder.size()
-                               + (int)qubits_init_null.size() + (int)qubits_init_repeat.size()
-                               + (int)qubits_superfluous_flags.size() + qubits_beam_metric.size()
-                               + (int)qubits_ancilla_pool.size() +(int)evaluation_qubits.size());
-  quantum_decoder_algo->execute(buffer);
-  auto info = buffer->getInformation();
-//  buffer->print();
-//  EXPECT_GT(BestScore, 0);
+  
+  check_output_strings(probability_table, qubits_string, 
+                           {"01", "10"}, {}, {}) ;
+  
 
 }
 
